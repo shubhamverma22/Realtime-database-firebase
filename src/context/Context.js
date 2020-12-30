4c8b66ff5f089969280a90bfe434dcef44c083ea
Shubham Verma <lpushubham98@gmail.com>
2020-12-30 12:43:18 +0530

    firebase commit

---
+++ src/context/Context.js
@@ -0,0 +1,5 @@
+//TODO: DONE Create context: ContactContext
+
+import { createContext } from "react";
+
+export const ContactContext = createContext();
---
+++ src/utils/config.js
@@ -0,0 +1,18 @@
+//TODO: DONE add firebase configuration and export it
+export const firebaseConfig = {
+  apiKey: "AIzaSyCg7yQt1nbLnsJuzEq1sJgGvCQWGeEWR8o",
+  authDomain: "demofirebase-6badb.firebaseapp.com",
+  databaseURL: "https://demofirebase-6badb-default-rtdb.firebaseio.com",
+  projectId: "demofirebase-6badb",
+  storageBucket: "demofirebase-6badb.appspot.com",
+  messagingSenderId: "468029419178",
+  appId: "1:468029419178:web:d6a794b720708c294f2ad3",
+  measurementId: "G-FPXS9DTZRR"
+};
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
